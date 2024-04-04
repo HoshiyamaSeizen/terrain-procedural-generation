e9823b32db53af1fdade287539e24b9179ed1609
HoshiyamaSeizen <nazarafanasyev@gmail.com>
2024-04-04 13:33:01 +0300

    wave surface

---
+++ src/algorithms/WaveSurface.ts
@@ -0,0 +1,17 @@
+export const waveSurface = (
+	vertices: number[],
+	gridSize: number,
+	stepSize = 1,
+	amplitude = 10,
+	frequency = 0.1
+): void => {
+	const calculateY = (x: number, z: number): number =>
+		Math.sin(x * frequency) * Math.cos(z * frequency) * amplitude;
+
+	for (let x = -gridSize / 2; x <= gridSize / 2; x += stepSize) {
+		for (let z = -gridSize / 2; z <= gridSize / 2; z += stepSize) {
+			const y = calculateY(x, z);
+			vertices.push(x, y, z);
+		}
+	}
+};
--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import Box from '../meshes/Box';
+import Surface from '../meshes/Surface';
+import { OrbitControls, Stats } from '@react-three/drei';
+import Environment from '../Environment';
 
 const Screen = () => {
 	return (
 		<div className="canvas-container">
-			<Canvas>
-				<ambientLight intensity={Math.PI / 2} />
-				<spotLight
-					position={[10, 10, 10]}
-					angle={0.15}
-					penumbra={1}
-					decay={0}
-					intensity={Math.PI}
-				/>
-				<pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-				<Box position={[-1.2, 0, 0]} />
-				<Box position={[1.2, 0, 0]} />
+			<Canvas camera={{ position: [30, 10, 30] }}>
+				<Environment />
+				<Surface position={[0, 0, 0]} scale={0.5} />
+				<OrbitControls target={[0, 0, 0]} />
+				<Stats className="stats" />
 			</Canvas>
 		</div>
 	);
